3db023017a5030a66d153d231e855b27eefa89f2
andycvv <135892408+andycvv@users.noreply.github.com>
2024-02-06 12:45:00 -0500

    Add first class

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+import { TwitterFollowCard } from './TwitterFollowCard';
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Heraldo',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  },
+]
+
+export function App() {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing })=>
+          (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          )
+        )
+      }
+    </section>
+  );
+}
